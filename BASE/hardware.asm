8c4e44c8e2633caf81d4e69d1d9ceb4264f872c4
dogdark <a8dogdark@gmail.com>
2020-04-01 17:48:08 -0300

    mod dogdark 01-04-2020 17:47

--- BASE/hardware.asm
+++ BASE/hardware.asm
@@ -2,8 +2,16 @@
 ; del Hardware del ATARI XL-XE
 ;-------------------------------------
 
-SDLSTL = $230
-STRIG0 = $284
-COLOR0 = $2c4 
-CHBAS  = $2f4
-CONSOL = $d01f
+SDLSTL 	= 	$230
+STRIG0 	= 	$284
+CHBAS  	= 	$2f4
+CONSOL 	= 	$d01f
+PCOLR0	=	$2C0
+PCOLR1	=	$2C1
+PCOLR2	=	$2C2
+PCOLR3	=	$2C3
+COLOR0 	= 	$2C4
+COLOR1 	= 	$2C5
+COLOR2 	= 	$2C6
+COLOR3 	= 	$2C7
+COLOR4 	= 	$2C8
