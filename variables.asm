a6f3c8a86ca54ec050960a0252607f077770b885
AsCrNet <ascrsoft@gmail.com>
2020-04-01 17:04:11 -0300

    Definición de variables

---
+++ variables.asm
@@ -0,0 +1,19 @@
+; Declaración de variables del juegos
+; Página cero 
+; $80 a $FF usadas por el BASIC 
+;--------------------------------------
+
+    org $80
+
+; Dirección del movimiento  
+; 0 = sin movimiento
+; 1 = arriba
+; 2 = abajo
+; 3 = izquiera
+; 4 = derecha
+direccion .ds 1
+
+; Posición X e Y de la pelota en pantalla
+pelota_x  .ds 1
+pelota_y  .ds 1
+
