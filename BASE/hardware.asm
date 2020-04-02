7913151a45b2c05f6b6850d5ca1e27ca0382cf75
AsCrNet <ascrsoft@gmail.com>
2020-04-01 21:26:23 -0300

    Cambios título y puzzle

--- BASE/hardware.asm
+++ BASE/hardware.asm
@@ -2,16 +2,17 @@
 ; del Hardware del ATARI XL-XE
 ;-------------------------------------
 
-SDLSTL 	= 	$230
-STRIG0 	= 	$284
-CHBAS  	= 	$2f4
-CONSOL 	= 	$d01f
-PCOLR0	=	$2C0
-PCOLR1	=	$2C1
-PCOLR2	=	$2C2
-PCOLR3	=	$2C3
-COLOR0 	= 	$2C4
-COLOR1 	= 	$2C5
-COLOR2 	= 	$2C6
-COLOR3 	= 	$2C7
-COLOR4 	= 	$2C8
+SDLSTL = $230
+STRIG0 = $284
+PCOLR0 = $2c0
+PCOLR1 = $2c1
+PCOLR2 = $2c2
+PCOLR3 = $2c3
+COLOR0 = $2c4
+COLOR1 = $2c5
+COLOR2 = $2c6
+COLOR3 = $2c7
+COLOR4 = $2c8
+CHBAS  = $2f4
+CONSOL = $d01f
+
