cf2b5ef749ceebb079c90c041b862621a1824cc8
dogdark <a8dogdark@gmail.com>
2020-04-22 01:56:12 -0400

    se modifica data de display nivel0

--- niveles.asm
+++ niveles.asm
@@ -54,17 +54,40 @@ nivel_temp
 
 nivel0
     .by 0,0,0,0
-    dta '                    '
-    dta '               1    '
-    dta '                    '
-    dta '    2               '
-    dta '    3               '
-    dta '    3               '
-    dta '    3               '
-    dta '    4    56667      '
-    dta '                    '
-    dta '                    '
-    dta '      11111111      '
+;primero va la posicion y despues el tipo de dibujo
+;segun numero 1 - 2 - 3 - 8 - 9 etc
+;originalmente 214 valores versus 38
+;
+    dta 36,1	;21-40
+    dta 65,2,85,3,105,3,125,3,145,4		;desde 61
+    dta 150,5,151,6,152,6,153,6,154,7	;hasta 160
+    dta 207,1,208,1,209,1,210,1			;desde 201
+    dta 211,1,212,1,213,1,214,1			;hasta 220
+;    dta '12345678901234567890';1-20
+;    dta '               1    ';21-40   1 en 36
+;    dta '                    ';41-60   ceros
+;    dta '    2               ';61-80   2 en 65
+;    dta '    3               ';81-100  3 en 85
+;    dta '    3               ';101-120 3 en 105
+;    dta '    3               ';121-140 3 en 125
+;    dta '    4    56667      ';141-160 4 en 145 5 en 150 6 en 151 al 153 7 en 154
+;    dta '                    ';161-180 ceros
+;    dta '                    ';181-200 ceros
+;    dta '      11111111      ';201-220 1 en 207 al 214
+    
+;nivel0
+;    .by 0,0,0,0
+;    dta '                    '
+;    dta '               1    '
+;    dta '                    '
+;    dta '    2               '
+;    dta '    3               '
+;    dta '    3               '
+;    dta '    3               '
+;    dta '    4    56667      '
+;    dta '                    '
+;    dta '                    '
+;    dta '      11111111      '
 
 nivel1
     .by 0,0,0,0
